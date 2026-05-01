71bd29c9bdebee14cce8424cb3d5f0ebe261f422
kossinge-cisco <kossinge+gh@cisco.com>
2026-04-30 22:03:37 -0700

    Fixed configurations to configuration

--- modules/cml2-foundations-lab/main.tf
+++ modules/cml2-foundations-lab/main.tf
@@ -84,7 +84,7 @@ resource "cml2_node" "ext-conn-0" {
   x              = 680
   y              = 120
   tags           = ["external_connector"]
-  configurations = "bridge0"
+  configuration = "bridge0"
 }
 
 resource "cml2_link" "l0" {
